364ed7033038be6a05c62d3b06cba3e113632140
deva <deva33.rd5@gmail.com>
2022-12-22 16:07:04 +0530

    Auto Login Changes

--- application/controllers/Autologin.php
+++ application/controllers/Autologin.php
@@ -26,28 +26,35 @@ class Autologin extends CI_Controller
 
     public function userlogin($enc_data)
     {
-        $student_data = $this->usersupport->autologin_encrypt_decrypt('decrypt', $enc_data);
-        $tudent_dat_ary = explode('-', $student_data);
-        $student_regno  = $tudent_dat_ary[0];
-        $student_dob  =  date('Y-m-d', strtotime($tudent_dat_ary[1]));
-        $student_data = $this->autologin_model->get_student_data($student_regno, $student_dob);
-        if ($student_data != FALSE) {
-            $sess_array = array(
-                'student_id' => $student_data['student_id'],
-                'student_name' => $student_data['student_name'],
-                'student_barcode' => $student_data['student_barcode'],
-                'course_name' => $student_data['course_name'],
-                'batch' => $student_data['batch'],
-                'status' => $student_data['status'],
-            );
-            $this->session->set_userdata('srihertemp_user_logged_in', $sess_array);
-            redirect(base_url('dashboard'), 'refresh');
+        if (!empty($enc_data)) {
+            $student_data = $this->usersupport->autologin_encrypt_decrypt('decrypt', $enc_data);
+            $tudent_dat_ary = explode('_', $student_data);
+            if (is_array($tudent_dat_ary)) {
+                $student_regno  = $tudent_dat_ary[0];
+                $student_dob  =  date('Y-m-d', strtotime($tudent_dat_ary[1]));
+                $student_data = $this->autologin_model->get_student_data($student_regno, $student_dob);
+                if ($student_data != FALSE) {
+                    $sess_array = array(
+                        'student_id' => $student_data['student_id'],
+                        'student_name' => $student_data['student_name'],
+                        'student_barcode' => $student_data['student_barcode'],
+                        'course_name' => $student_data['course_name'],
+                        'batch' => $student_data['batch'],
+                        'status' => $student_data['status'],
+                    );
+                    $this->session->set_userdata('srihertemp_user_logged_in', $sess_array);
+                    redirect(base_url('dashboard'), 'refresh');
+                } else {
+                    $this->session->set_flashdata('db_error', 'Data Not Available.');
+                    redirect(base_url(), 'refresh');
+                }
+            } else {
+                $this->session->set_flashdata('db_error', 'Data Not Available.');
+                redirect(base_url(), 'refresh');
+            }
         } else {
-            $data = '';
-            $this->load->view('includes/header', $data);
-            $this->load->view('includes/menu', $data);
-            $this->load->view('pages/credential_errorpage', $data);
-            $this->load->view('includes/footer', $data);
+            $this->session->set_flashdata('db_error', 'Data Not Available.');
+            redirect(base_url(), 'refresh');
         }
     }
 }
